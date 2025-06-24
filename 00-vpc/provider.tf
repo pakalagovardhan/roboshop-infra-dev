583725e2e9a9952551d0059bf10b905655ee9686
pakalagovardhan <p.govardhan321@gmail.com>
2025-06-24 15:54:31 +0530

    coding

---
+++ 00-vpc/provider.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.98.0"
+    }
+  }
+  backend "s3" {
+    bucket = "dev1407-dev"
+    key    = "roboshop-dev"
+    region = "us-east-1"
+    encrypt      = true  
+    use_lockfile = true  #S3 native locking
+    # dynamodb_table = "dev1407-remote-state"
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
---
+++ 10-sg/main.tf
@@ -0,0 +1,14 @@
+module "frontend" {
+    # source = "../../terraform-aws-securitygroup"
+    source = "git::https://github.com/pakalagovardhan/terraform-aws-securitygroup.git?ref=main"
+    project = var.project
+    environment = var.environment
+    sg_name = var.frontend_sg_name
+    sg_description = var.frontend_sg_description
+    vpc_id = local.vpc_id
+
+}
+
+# output "sg_id" {
+#     value = module.frontend.sg_id
+# }
