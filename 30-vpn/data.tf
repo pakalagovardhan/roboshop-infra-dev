e9cfef9a3190b951e61d4f27582fd47618d8240c
pakalagovardhan <p.govardhan321@gmail.com>
2025-07-08 17:57:07 +0530

    added

---
+++ 30-vpn/data.tf
@@ -0,0 +1,18 @@
+data "aws_ami" "openvpn" {
+  owners = ["679593333241"]
+  most_recent = true
+  filter {
+      name = "name"
+      values = ["OpenVPN Access Server Community Image-8fbe3379-*"]
+  }
+}
+
+
+
+data "aws_ssm_parameter" "vpn_sg_id" {
+  name = "/${var.project}/${var.environment}/vpn_sg_id"
+}
+
+data "aws_ssm_parameter" "public_subnet_ids" {
+  name = "/${var.project}/${var.environment}/public_subnet_ids"
+}
