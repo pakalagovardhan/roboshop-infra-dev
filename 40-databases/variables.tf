6a9e07c1c9892caf0266f9c347bcabedde8fb91f
pakalagovardhan <p.govardhan321@gmail.com>
2025-07-08 17:57:30 +0530

    added

---
+++ 40-databases/variables.tf
@@ -0,0 +1,15 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id" {
+    default = "Z10025892IKNKTBDRCRYQ"
+}
+
+variable "zone_name" {
+    default = "dev1407.site"
+}
